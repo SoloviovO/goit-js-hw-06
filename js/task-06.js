9bd62c3cb017a4062fee59289a25b3e660f817d5
SoloviovO <soloviov.o.m@gmail.com>
2023-01-11 16:30:27 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,10 @@
 const inputEl = document.querySelector('#validation-input');
-const lengthInputText = inputEl.dataset.length;
+const lengthInputText = Number(inputEl.dataset.length);
 
 inputEl.addEventListener('blur', onChangeInputFocus);
 
 function onChangeInputFocus(event) {
-  if (event.currentTarget.value.length == lengthInputText) {
+  if (event.currentTarget.value.length === lengthInputText) {
     inputEl.classList.add('valid');
     inputEl.classList.remove('invalid');
   } else {
